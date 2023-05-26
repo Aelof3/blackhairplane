67e1122732b58d5bc8ad4b1fb7ed05b60ed036dd
Richard Folea <Folea3@gmail.com>
2023-05-26 18:59:52 -0400

    image optimizations again

--- app/page.js
+++ app/page.js
@@ -71,6 +71,7 @@ export default function Home() {
               width={50}
               height={50}
               alt="Picture of a black airplane with hair"
+              blurDataURL='data:image/jpeg;base64,/9j/4AAQSkZJRgABAQAAAQABAAD/2wCEAAkGBxMTEhUTExMWFhUXGBgYGBgYGBgYGBgYGBgYGBgYGBgYHSggGBolHRgXITEhJSkrLi4uGB8zODMsNygtLisBC'
               placeholder='blur'
             />
           </div>
