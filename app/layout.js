028cf44ac8979160eeee2f735660ab6735c9f172
Richard Folea <Folea3@gmail.com>
2023-05-26 23:19:49 -0400

    og updates

--- app/layout.js
+++ app/layout.js
@@ -11,6 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta property="og:url" content="https://blackhairplane.com/"/>
+        <meta property="og:type" content="website"/>
+        <meta property="og:title" content="Black Hairplane"/>
+        <meta property="og:description" content="blackairplane.com"/>
+        <meta property="og:image" content="https://blackhairplane.com/og-1024x576.png"/>
+        <meta name="twitter:card" content="summary_large_image"/>
+        <meta property="twitter:domain" content="blackhairplane.com"/>
+        <meta property="twitter:url" content="https://blackhairplane.com/"/>
+        <meta name="twitter:title" content="Black Hairplane"/>
+        <meta name="twitter:description" content="blackairplane.com"/>
+        <meta name="twitter:image" content="https://blackhairplane.com/og-1024x576.png"/>
+      </head>
+              
       <body className={inter.className}>{children}</body>
     </html>
   )
